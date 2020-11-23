d569af04fb710927f15fcdf9f02b301dd27131ae
Sergey <seregqaaa@yandex.ru>
2020-11-23 16:58:05 +0500

    UI components decomposition done, BL todo

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,55 +1,33 @@
 <template>
   <div class="wrapper">
     <main class="main">
-      <div class="banner">
-        <img
-          src="https://scontent-arn2-2.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/121166260_823691025121572_1035172798211476107_n.jpg?_nc_ht=scontent-arn2-2.cdninstagram.com&_nc_cat=100&_nc_ohc=IG8vlX-bqnQAX-KBRoe&tp=1&oh=2e8296e38fb6bbf8d2e3613307f4e397&oe=5FE2E7A3"
-          alt="Oleg"
-          class="banner-img"
-        />
-        <div class="avatar-wrapper">
-          <img
-            src="https://scontent-arn2-2.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/121166260_823691025121572_1035172798211476107_n.jpg?_nc_ht=scontent-arn2-2.cdninstagram.com&_nc_cat=100&_nc_ohc=IG8vlX-bqnQAX-KBRoe&tp=1&oh=2e8296e38fb6bbf8d2e3613307f4e397&oe=5FE2E7A3"
-            alt="Oleg"
-            class="avatar"
-          />
-          <div class="person-wrapper">
-            <h2 class="person-name">&nbsp;Олег</h2>
-            <span class="person-age">, 19</span>
-          </div>
-        </div>
-      </div>
+      <profile-avatar :imageLink="user.imageLink" :userName="user.name" :userAge="user.age"></profile-avatar>
       <div class="main-body">
         <section class="section section-actions">
           <div class="buttons-wrapper">
-            <button id="block" class="btn btn-rounded"><i class="material-icons">block</i></button>
-            <button id="like" class="btn btn-rounded"><i class="material-icons-outlined">thumb_up</i></button>
-            <button id="message" class="btn btn-rounded"><i class="material-icons-outlined">create</i></button>
-            <button id="skip" class="btn btn-rounded"><i class="material-icons">arrow_forward</i></button>
+            <profile-button
+              v-for="button in buttons"
+              :key="button.id"
+              :buttonId="button.id"
+              :buttonType="button.type"
+              :isOutlined="button.isOutlined"
+            />
           </div>
         </section>
         <section class="section section-about">
-          <h4 class="section-title">Обо мне:</h4>
-          <p class="person-about">
-            Повседневная практика показывает, что сложившаяся структура организации требует от нас системного анализа
-            ключевых компонентов планируемого обновления. Задача организации, в особенности же реализация намеченного
-            плана развития требует от нас системного анализа системы обучения кадров, соответствующей насущным
-            потребностям! Значимость этих проблем настолько очевидна, что новая модель организационной деятельности
-            создаёт предпосылки качественно новых шагов для системы масштабного изменения ряда параметров? Не следует,
-            однако, забывать о том, что дальнейшее развитие различных форм деятельности в значительной степени
-            обуславливает создание направлений прогрессивного развития?
-          </p>
+          <h4 class="section-title">{{ titles.about }}</h4>
+          <p class="person-about">{{ user.about }}</p>
         </section>
         <section class="section section-interests">
-          <h4 class="section-title">Интересы:</h4>
+          <h4 class="section-title">{{ titles.interests }}</h4>
           <ul class="person-chips person-tags">
-            <profile-chip v-for="interest in userInterests" :key="interest" :chipText="interest"></profile-chip>
+            <profile-chip v-for="interest in user.interests" :key="interest" :chipText="interest"></profile-chip>
           </ul>
         </section>
         <section class="section section-music">
-          <h4 class="section-title">Я слушаю:</h4>
+          <h4 class="section-title">{{ titles.music }}</h4>
           <ul class="person-chips person-music">
-            <profile-chip v-for="artist in userMusic" :key="artist" :chipText="artist"></profile-chip>
+            <profile-chip v-for="artist in user.music" :key="artist" :chipText="artist"></profile-chip>
           </ul>
         </section>
       </div>
@@ -60,14 +38,49 @@
 
 <script>
 import AppMenu from '@/components/AppMenu'
-import ProfileChip from '../components/ProfileChip.vue'
+import ProfileChip from '@/components/ProfileChip'
+import ProfileButton from '@/components/ProfileButton'
+import ProfileAvatar from '@/components/ProfileAvatar'
 
 export default {
-  components: { AppMenu, ProfileChip },
+  components: { AppMenu, ProfileChip, ProfileButton, ProfileAvatar },
   data() {
     return {
-      userInterests: ['Football', 'CS:GO', 'Sport', 'Cars', 'IT'],
-      userMusic: ['LSP', 'Kanye West']
+      titles: {
+        about: 'Обо мне:',
+        interests: 'Интересы:',
+        music: 'Я слушаю:'
+      },
+      user: {
+        name: 'Кидала',
+        age: 33,
+        imageLink:
+          'https://sun9-22.userapi.com/impf/vX8WjY7YpW-_ju0zJzIyKFARF8VUybaJqLGxIw/x8c8-vuseZg.jpg?size=960x960&quality=96&proxy=1&sign=d196759d085c6005493a8ff2814c07a7',
+        about:
+          'Повседневная практика показывает, что сложившаяся структура организации требует от нас системного анализа ключевых компонентов планируемого обновления. Задача организации, в особенности же реализация намеченного плана развития требует от нас системного анализа системы обучения кадров, соответствующей насущным потребностям! Значимость этих проблем настолько очевидна, что новая модель организационной деятельности создаёт предпосылки качественно новых шагов для системы масштабного изменения ряда параметров? Не следует, однако, забывать о том, что дальнейшее развитие различных форм деятельности в значительной степени обуславливает создание направлений прогрессивного развития?',
+        interests: ['Football', 'CS:GO', 'Sport', 'Cars', 'IT'],
+        music: ['LSP', 'Kanye West']
+      },
+      buttons: [
+        {
+          type: 'block',
+          id: 'block'
+        },
+        {
+          type: 'thumb_up',
+          id: 'like',
+          isOutlined: true
+        },
+        {
+          type: 'create',
+          id: 'message',
+          isOutlined: true
+        },
+        {
+          type: 'arrow_forward',
+          id: 'skip'
+        }
+      ]
     }
   }
 }
